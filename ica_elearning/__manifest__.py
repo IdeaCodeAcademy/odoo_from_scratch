2c4bdfb1446428cc25bbdd2e30897d6fe5e1b9f9
Agga <hanzawnyein@Hans-MacBook-Air.local>
2024-09-22 15:39:49 +0700

    add manifest

--- ica_elearning/__manifest__.py
+++ ica_elearning/__manifest__.py
@@ -1,3 +1,10 @@
 {
     "name":"ICA E-Learning",
+    "license":"LGPL-3",
+    "author":"Han Zaw Nyein",
+    "category":"Services",
+    "depends":["base","web"],
+    "demo":[],
+    "data":[],
+    "assets":{},
 }
